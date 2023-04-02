ab9c1887c2b6da3b191110a45e1facf1c0f26c93
1E10 Laptop <sample@bths.edu>
2023-04-01 21:47:07 -0400

    Changes by Pushe

--- src/MovePromoted.java
+++ src/MovePromoted.java
@@ -61,37 +61,63 @@ public class MovePromoted extends Move {
     }
 
     public boolean winCondition(Checker[][] checkerPieces) {
-        boolean allCanMove = false;
+        if((containsRed(checkerPieces) && containsBlack(checkerPieces)) == false) {
+            return false;
+        }
         for (int i = 0; i < 8; i++) {
             for (int y = 0; y < 8; y++) {
                 if (blackMove) { //if it is black to move, check the legal moves for all black checkers
                     if (checkerPieces[i][y].getColor().equals("black")){
                         if(checkerPieces[i][y].isPromoted()) {
-                            if ((canMoveLeftBlack(checkerPieces, checkerPieces[i][y]) && canMoveRightBlack
+                            if ((canMoveLeftBlack(checkerPieces, checkerPieces[i][y]) || canMoveRightBlack
                                     (checkerPieces, checkerPieces[i][y])) || (capturePromoted.canCaptureLeftBlack
-                                    (checkerPieces, checkerPieces[i][y]) && (capturePromoted.canCaptureRightBlack(checkerPieces, checkerPieces[i][y])))) { //if black piece has any regular moves
-                                allCanMove = true;
+                                    (checkerPieces, checkerPieces[i][y]) || (capturePromoted.canCaptureRightBlack(checkerPieces, checkerPieces[i][y])))) { //if black piece has any regular moves
+                                return true;
                             }
-                        } else if (canMoveLeftBlack(checkerPieces, checkerPieces[i][y]) && canMoveRightBlack(checkerPieces, checkerPieces[i][y])) {
-                            allCanMove = true;
+                        } else if (super.canMoveLeftBlack(checkerPieces, checkerPieces[i][y]) || super.canMoveRightBlack(checkerPieces, checkerPieces[i][y])) {
+                            return true;
                         }
                     }
-                } else if(checkerPieces[i][y].getColor().equals("red")) {
-                    if(checkerPieces[i][y].isPromoted()){
-                        if((canMoveLeftRed(checkerPieces, checkerPieces[i][y]) && canMoveRightRed
-                                (checkerPieces, checkerPieces[i][y])) || (capturePromoted.canCaptureLeftRed
-                                (checkerPieces, checkerPieces[i][y]) && (capturePromoted.canCaptureRightRed(checkerPieces, checkerPieces[i][y])))) {
-                            allCanMove = true;
+                } else {
+                    if(checkerPieces[i][y].getColor().equals("red")) {
+                        if(checkerPieces[i][y].isPromoted()){
+                            if((canMoveLeftRed(checkerPieces, checkerPieces[i][y]) || canMoveRightRed
+                                    (checkerPieces, checkerPieces[i][y])) || (capturePromoted.canCaptureLeftRed
+                                    (checkerPieces, checkerPieces[i][y]) || (capturePromoted.canCaptureRightRed(checkerPieces, checkerPieces[i][y])))) {
+                                return true;
+                            }
                         }
-                    }
-                    if (canMoveLeftRed(checkerPieces, checkerPieces[i][y]) && canMoveRightRed(checkerPieces, checkerPieces[i][y])) {
-                        allCanMove = true;
+                        if (super.canMoveLeftRed(checkerPieces, checkerPieces[i][y]) || super.canMoveRightRed(checkerPieces, checkerPieces[i][y])) {
+                            return true;
+                        }
                     }
                 }
             }
         }
-        return allCanMove;
+        return false;
     }
 
+    private boolean containsRed(Checker[][] checkerPieces) {
+        boolean contains = false;
+        for (int i = 0; i < 8; i++) {
+            for (int y = 0; y < 8; y++) {
+                if (checkerPieces[i][y].getColor().equals("red")) {
+                    contains = true;
+                }
+            }
+        }
+        return contains;
+    }
 
+    private boolean containsBlack(Checker[][] checkerPieces) {
+        boolean contains = false;
+        for (int i = 0; i < 8; i++) {
+            for (int y = 0; y < 8; y++) {
+                if (checkerPieces[i][y].getColor().equals("black")) {
+                    contains = true;
+                }
+            }
+        }
+        return contains;
+    }
 }
