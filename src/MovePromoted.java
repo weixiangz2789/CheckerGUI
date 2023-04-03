de3e3b5f43c5ed7aa02c3836619ff0786d3e3dd8
1E10 Laptop <sample@bths.edu>
2023-04-02 22:47:26 -0400

    w

--- src/MovePromoted.java
+++ src/MovePromoted.java
@@ -66,45 +66,6 @@ public class MovePromoted extends Move {
         return false;
     }
 
-//    public boolean winCondition(Checker[][] checkerPieces) {
-//        if((containsRed(checkerPieces) == false) || (containsBlack(checkerPieces)) == false) {
-//            return false;
-//        }
-//
-//
-//        for (int i = 0; i < 8; i++) {
-//            for (int y = 0; y < 8; y++) {
-//                if (blackMove) { //if it is black to move, check the legal moves for all black checkers
-//                    if (checkerPieces[i][y].getColor().equals("black")){
-//                        if(checkerPieces[i][y].isPromoted()) {
-//                            if ((canMoveLeftBlack(checkerPieces, checkerPieces[i][y]) || canMoveRightBlack
-//                                    (checkerPieces, checkerPieces[i][y])) || (capturePromoted.canCaptureLeftBlack
-//                                    (checkerPieces, checkerPieces[i][y]) || (capturePromoted.canCaptureRightBlack(checkerPieces, checkerPieces[i][y])))) { //if black piece has any regular moves
-//                                return true;
-//                            }
-//                        } else if (super.canMoveLeftBlack(checkerPieces, checkerPieces[i][y]) || super.canMoveRightBlack(checkerPieces, checkerPieces[i][y])) {
-//                            return true;
-//                        }
-//                    }
-//                } else {
-//                    if(checkerPieces[i][y].getColor().equals("red")) {
-//                        if(checkerPieces[i][y].isPromoted()) {
-//                            if ((canMoveLeftRed(checkerPieces, checkerPieces[i][y]) || canMoveRightRed
-//                                    (checkerPieces, checkerPieces[i][y])) || (capturePromoted.canCaptureLeftRed
-//                                    (checkerPieces, checkerPieces[i][y]) || (capturePromoted.canCaptureRightRed(checkerPieces, checkerPieces[i][y])))) {
-//                                return true;
-//                            }
-//
-//                        } else if (super.canMoveLeftRed(checkerPieces, checkerPieces[i][y]) || super.canMoveRightRed(checkerPieces, checkerPieces[i][y]) ) {
-//                            return true;
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//        return false;
-//    }
-
     //false = end true = continue
     public boolean winCondition(Checker[][] checkerPieces) {
         if ((containsRed(checkerPieces) == false) || (containsBlack(checkerPieces)) == false) {
@@ -130,19 +91,21 @@ public class MovePromoted extends Move {
                         }
                     }
                 } else { //red
-                    if (checkerPieces[i][y].isPromoted()) { //promoted red
-                        canMoveLeftRed(checkerPieces, checkerPieces[i][y]);
-                        canMoveRightRed(checkerPieces, checkerPieces[i][y]);
-                        capturePromoted.canCaptureLeftRed(checkerPieces, checkerPieces[i][y]);
-                        capturePromoted.canCaptureRightRed(checkerPieces, checkerPieces[i][y]);
-                    } else { //non promoted red
-                        super.canMoveLeftRed(checkerPieces, checkerPieces[i][y]);
-                        super.canMoveRightRed(checkerPieces, checkerPieces[i][y]);
-                        capture.canCaptureLeftRed(checkerPieces, checkerPieces[i][y]);
-                        capture.canCaptureRightRed(checkerPieces, checkerPieces[i][y]);
-                    }
-                    if (getLegalMovesRed().size() > 0) {
-                        return true;
+                    if (checkerPieces[i][y].getColor().equals("red")) {
+                        if (checkerPieces[i][y].isPromoted()) { //promoted red
+                            canMoveLeftRed(checkerPieces, checkerPieces[i][y]);
+                            canMoveRightRed(checkerPieces, checkerPieces[i][y]);
+                            capturePromoted.canCaptureLeftRed(checkerPieces, checkerPieces[i][y]);
+                            capturePromoted.canCaptureRightRed(checkerPieces, checkerPieces[i][y]);
+                        } else { //non promoted red
+                            super.canMoveLeftRed(checkerPieces, checkerPieces[i][y]);
+                            super.canMoveRightRed(checkerPieces, checkerPieces[i][y]);
+                            capture.canCaptureLeftRed(checkerPieces, checkerPieces[i][y]);
+                            capture.canCaptureRightRed(checkerPieces, checkerPieces[i][y]);
+                        }
+                        if (getLegalMovesRed().size() > 0) {
+                            return true;
+                        }
                     }
                 }
             }
